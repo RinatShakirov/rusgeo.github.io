9514213f2b4bdc7635782a49cb644b85cff85e17
RinatShakirov <62038576+RinatShakirov@users.noreply.github.com>
2021-05-19 17:19:29 +0200

    Delete javascript directory

--- javascript/scripts.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function(){
-    const btn_toggle = document.querySelector('.toggl__btn');
-    const nav_show = document.querySelector('.nav_inner');
-    
-    btn_toggle.addEventListener('click', (e)=>{
-        nav_show.classList.add('active');
-    })
-
-});
